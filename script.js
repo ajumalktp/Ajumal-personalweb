91122536f1af3a5db92f40afc31c3c39124d0b76
ajumalktp <121402436+ajumalktp@users.noreply.github.com>
2023-01-03 11:21:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function sendMail(){
+    var params={
+        name:document.getElementById('name').value ,
+        email:document.getElementById('email').value,
+        message:document.getElementById('message').value
+    }
+    const serviceId="service_ff1ty64";
+    const templateId="template_iasbc97";
+    emailjs.send(serviceId,templateId,params)
+    .then(
+        res =>{
+            document.getElementById("name").value ="";
+            document.getElementById("email").value ="";
+            document.getElementById("message").value ="";
+            console.log(res);
+            alert("your message sent succesfully")
+        })
+        .catch((err) => console.log(err));
+}
